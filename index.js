8ca1a5c046ee8131b9b51e4f5a6cef39e7f6250e
antjuntu <antjuntu@gmail.com>
2018-10-31 19:49:49 +0200

    3.5

--- index.js
+++ index.js
@@ -55,6 +55,24 @@ app.get('/info', (request, response) => {
     )
 })
 
+const generateId = () => {
+  return Math.floor(Math.random() * 100000000)
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  }
+
+  dummyPersons = dummyPersons.concat(person)
+
+  response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
