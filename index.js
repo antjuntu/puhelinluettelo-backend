84f8e5da59a344c99f7f9a7dc0db286847773bb0
antjuntu <antjuntu@gmail.com>
2018-10-31 19:59:49 +0200

    3.6

--- index.js
+++ index.js
@@ -62,6 +62,14 @@ const generateId = () => {
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
+  if (body.name === undefined || body.number === undefined) {
+    return response.status(400).json({ error: 'name or number missing' })
+  }
+
+  if (dummyPersons.find(person => person.name === body.name)) {
+    return response.status(400).json({ error: 'name must be unique' })
+  }
+
   const person = {
     name: body.name,
     number: body.number,
