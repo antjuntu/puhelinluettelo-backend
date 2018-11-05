ee3111086a658e6552362bea4cc38f2a6cd30bb7
antjuntu <antjuntu@gmail.com>
2018-11-05 15:23:40 +0200

    Production db created for heroku

--- models/person.js
+++ models/person.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose')
-const { url } = require('../db')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const url = process.env.MONGODB_URI
 
 mongoose.connect(url, { useNewUrlParser: true })
 
